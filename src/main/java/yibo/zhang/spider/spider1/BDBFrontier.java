6de2eca920a0f1303482ce400be428f6c2c7d425
goldeneyezhang <goldeneyezhang@163.com>
2017-06-07 15:22:10 +0800

    berkeley db

--- src/main/java/yibo/zhang/spider/spider1/BDBFrontier.java
+++ src/main/java/yibo/zhang/spider/spider1/BDBFrontier.java
@@ -1,9 +1,16 @@
 package yibo.zhang.spider.spider1;
 
+import java.io.FileNotFoundException;
+import java.util.Map.Entry;
+import java.util.Set;
+
 import com.sleepycat.bind.EntryBinding;
 import com.sleepycat.bind.serial.SerialBinding;
+import com.sleepycat.collections.StoredMap;
+import com.sleepycat.je.DatabaseException;
 
 public class BDBFrontier extends AbstractFrontier implements Frontier{
+	private StoredMap pendingUrisDB=null;
 	//使用默认的路径和缓存大小构造函数
 	public BDBFrontier(String homeDirectory) throws DatabaseException,FileNotFoundException{
 		super(homeDirectory);
@@ -15,6 +22,48 @@ public class BDBFrontier extends AbstractFrontier implements Frontier{
 	//获得下一条记录
 	public CrawlUrl getNext() throws Exception{
 		CrawlUrl result=null;
-		
+		if(!pendingUrisDB.isEmpty()){
+			Set entrys=pendingUrisDB.entrySet();
+			System.out.println(entrys);
+			Entry<String,CrawlUrl> entry=(Entry<String,CrawlUrl>)pendingUrisDB.entrySet().iterator().next();
+			result=entry.getValue();
+			delete(entry.getKey());
+		}
+		return result;
+	}
+	//存入URL
+	public boolean putUrl(CrawlUrl url){
+		put(url.getOriUrl(),url);
+		return true;
+	}
+	//存入数据库的方法
+	protected void put(Object key,Object value){
+		pendingUrisDB.put(key, value);
+	}
+	//取出
+	protected Object get(Object key){
+		return pendingUrisDB.get(key);
+	}
+	//删除
+	protected Object delete(Object key){
+		return pendingUrisDB.remove(key);
+	}
+	//根据URL计算键值，可以使用各种压缩算法，包括MD5法
+	private String caculatedUrl(String url){
+		return url;
+	}
+	//测试函数
+	public static void main(String[] strs){
+		try{
+			BDBFrontier bBDBFrontier=new BDBFrontier("e:\\javacode\\spider1");
+			CrawlUrl url=new CrawlUrl();
+			url.setOriUrl("http://163.com");
+			bBDBFrontier.putUrl(url);
+			System.out.println(url);
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			
+		}
 	}
 }
